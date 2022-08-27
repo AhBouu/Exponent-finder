29e198f685c3f254154ca25b5a7f040150d4c23a
AhBouu <109218706+AhBouu@users.noreply.github.com>
2022-08-27 15:03:47 +0700

    build Exponent Finder

---
+++ index.js
@@ -0,0 +1,65 @@
+let inputNum = document.querySelector('.numInput');
+let resultNum = document.querySelector('.numResult');
+let btn = document.querySelector('.result');
+let resultText = document.querySelector('.resultDiv');
+let btn2 = document.querySelector('.clearResult');
+let exponent = 1;
+let p = document.querySelector('.resultP');
+btn.addEventListener('click', (oldNum, newNum, result) => {
+    oldNum = +resultNum.value;
+    newNum = +resultNum.value;
+    let msg = true;
+    p.textContent = +inputNum.value;
+    for (i=0;i<10;i++) {
+       exponent ++ 
+       result = newNum * oldNum;
+       newNum = result;
+       
+       if (+resultNum.value === 1) {
+        alert('Dont Enter Number 1')
+        
+        break
+        
+    };
+
+       if (result> +inputNum.value) {
+        if (msg) {
+            resultText.innerHTML += `<p class='exclamir'> This is the only possible answer!! </p>` ;
+        };
+
+       
+
+        
+        break;
+    };
+       resultText.innerHTML += `
+         <p class='resultP'> ${oldNum}^${exponent}  = ${result}</p>
+       `;
+
+       if (result === +inputNum.value) {
+        resultText.innerHTML += `<p class='exclamir'> This is the answer!! </p>` ;
+        msg = false
+       }
+    
+    
+    };
+    clear()
+});
+
+clear = () => {
+    result = 0;
+    inputNum.value = 0;
+    resultNum.value = 0;
+    oldNum = 0;
+    newNum = 0;
+    exponent = 1;
+};
+
+btn2.addEventListener('click', () => {
+    resultText.innerHTML = `
+    <p class="resultP">00</p>
+    `
+});
+
+
+
